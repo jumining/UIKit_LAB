dc0a1804e7a0193183ddd67a6d2543a6e047e798
임주민 <limj1213@gmail.com>
2023-04-14 19:59:18 +0900

    Add initial file for calendar and animation.

---
+++ practiceForSmeem/practiceForSmeem/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  practiceForSmeem
+//
+//  Created by 임주민 on 2023/04/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+}
